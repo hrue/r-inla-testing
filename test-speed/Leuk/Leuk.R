412f13487b46bed33d25ecb3b33243f964786f96
Haavard Rue <haavard.rue@kaust.edu.sa>
2021-10-29 22:45:34 +0300

    new

--- test-speed/Leuk/Leuk.R
+++ test-speed/Leuk/Leuk.R
@@ -1,23 +1,21 @@
 data(Leuk)
-## get the function to plot the map
-source(system.file("demodata/Leuk-map.R", package="INLA"))
 g = system.file("demodata/Leuk.graph", package="INLA")
 
-Leuk <- rbind(Leuk, Leuk)
-Leuk <- rbind(Leuk, Leuk)
-
-formula = inla.surv(Leuk$time, Leuk$cens) ~ sex + age +
+inla.setOption(scale.model = TRUE)
+Leuk$time <- Leuk$time / max(Leuk$time)
+formula = inla.surv(time, cens) ~ sex + age +
     f(inla.group(wbc), model="rw2")+
     f(inla.group(tpi), model="rw2")+
     f(district,model="besag",graph = g)
 
-inla.setOption(inla.call = "inla.mkl.work")
-inla.setOption(num.threads = "2:4")
-r = inla(formula, family="coxph", data=Leuk, twostage = FALSE, verbose = T)
-rr = inla(formula, family="coxph", data=Leuk, twostage = TRUE, verbose = T)
-rrr = inla(formula, family="coxph", data=Leuk, twostage = TRUE, verbose = T, keep = T, 
-           control.inla = list(control.twostage = list(stage1only = TRUE)))
-r$cpu
-rr$cpu
-rrr$cpu
+inla.setOption(num.threads = "8:1")
+##inla.setOption(inla.call = "remote")
+inla.setOption(inla.call = NULL)
+r = inla(formula, family="coxph", data=Leuk)
+rr = inla(formula, family="coxph", data=Leuk,
+          inla.mode = "experimental")
+print(round(dig = 2,
+            c(classic = r$cpu[2], 
+              experimental = rr$cpu[2], 
+              ratio = r$cpu[2]/rr$cpu[2])))
 
