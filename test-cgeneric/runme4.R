92343d01483677d21fdeec5919c4424885d18990
Haavard Rue <haavard.rue@kaust.edu.sa>
2022-02-09 22:18:57 +0300

    new

---
+++ test-cgeneric/runme4.R
@@ -0,0 +1,31 @@
+inla.setOption(inla.call = "inla.mkl.work")
+inla.setOption(inla.mode = "experimental")
+inla.setOption(num.threads = "1:1")
+
+system("make -B")
+n <- 5
+x <- arima.sim(n, model = list(ar = 0.9))
+x <- scale(x)
+s <- 0.01
+y <- x + rnorm(n, sd = s)
+
+## not entirely the same due to the boundary conditions, but almost as we scale 'y'
+r <- inla(
+    y ~ -1 + f(idx, model = "ar1",
+              hyper = list(prec = list(prior = "loggamma",
+                                       param = c(1, 1),
+                                       initial = 1),
+                           rho = list(prior = "normal",
+                                      param = c(0, 1),
+                                      initial = 1))), 
+    data = data.frame(y, idx = 1:n),
+    control.family = list(hyper = list(prec = list(initial = log(1/s^2), fixed = TRUE))))
+
+cmodel <- inla.cgeneric.define(model = "inla_cgeneric_ar1_model", shlib = "cgeneric-demo.so",
+                               n = n, debug = TRUE)
+rc <- inla(
+    y ~ -1 + f(idx, model = cmodel), 
+    data = data.frame(y, idx = 1:n),
+    control.family = list(hyper = list(prec = list(initial = log(1/s^2), fixed = TRUE))),
+    verbose = TRUE)
+
---
+++ test-cgeneric/runme5.R
@@ -0,0 +1,11 @@
+inla.setOption(inla.call = "inla.mkl.work")
+inla.setOption(inla.mode = "experimental")
+inla.setOption(num.threads = "1:1")
+
+system("make -B")
+n <- 5
+cmodel <- inla.cgeneric.define(model = "inla_cgeneric_ar1_model", shlib = "cgeneric-demo.so",n = n)
+##cmodel <- inla.cgeneric.define(model = "inla_cgeneric_iid_model", shlib = "cgeneric-demo.so",n = n)
+r <- inla.cgeneric.q(cmodel)
+
+
