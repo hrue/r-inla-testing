cb376326557d2b39131f878e21df1623d2663ed2
Haavard Rue <haavard.rue@kaust.edu.sa>
2022-02-09 23:06:02 +0300

    new

--- test-cgeneric/runme3.R
+++ test-cgeneric/runme3.R
@@ -4,7 +4,7 @@ inla.setOption(inla.mode = "classic")
 inla.setOption(inla.mode = "experimental")
 inla.setOption(num.threads = "4:1")
 inla.setOption(inla.call = "inla.mkl.work")
-inla.setOption(inla.call = "inla.valgrind")
+
 
 system("make -B")
 n <- 10
@@ -14,7 +14,7 @@ r <- inla(
     y ~ 1 + f(idx, model = "iid", hyper = list(prec = list(param = c(1, 1)))), 
     data = data.frame(y, idx = 1:n), verbose = T, 
     control.family = list(hyper = list(prec = list(initial = 12, fixed = TRUE))))
-stop("XXXXXXXXXXXXX")
+
 
 rmodel <- inla.rgeneric.define(inla.rgeneric.iid.model, n = n)
 rr <- inla(
@@ -23,7 +23,7 @@ rr <- inla(
     control.family = list(hyper = list(prec = list(initial = 12, fixed = TRUE))),
     verbose = !TRUE)
 
-cmodel <- inla.cgeneric.define(model = "inla_cgeneric_iid_model", shlib = "cgeneric-demo.so", n = n, debug = !FALSE,
+cmodel <- inla.cgeneric.define(model = "inla_cgeneric_iid_model", shlib = "cgeneric-demo.so", n = n, 
                                x = 1.0*(1:2),
                                ix = 1L:3L,
                                ch = "asd",
@@ -34,7 +34,7 @@ cmodel <- inla.cgeneric.define(model = "inla_cgeneric_iid_model", shlib = "cgene
 
 rc <- inla(
     y ~ 1 + f(idx, model = cmodel), 
-    data = data.frame(y = c(y, y), idx = rep(1:n, 2)),
+    data = data.frame(y = y, idx = 1:n),
     control.family = list(hyper = list(prec = list(initial = 12, fixed = TRUE))),
     verbose = TRUE)
 
