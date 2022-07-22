00f9eae143f52a991dfcdb4521de2c28bd200d7d
Haavard Rue <haavard.rue@kaust.edu.sa>
2022-07-22 19:26:04 +0200

    new

--- test-gcpo/runme5.R
+++ test-gcpo/runme5.R
@@ -1,4 +1,3 @@
-INLA:::inla.my.update(b = T)
 data(Tokyo)
 n <- nrow(Tokyo)
 formula = y ~ -1 + f(time, model="rw2", constr = F, cyclic=TRUE,
@@ -9,7 +8,7 @@ formula = y ~ -1 + f(time, model="rw2", constr = F, cyclic=TRUE,
 
 ## groups are list of list of nodes. NULL if none
 groups <- rep(list(list()), n)
-for(i in seq(1, n, by = 10)) {
+for(i in seq(1, n, by = 150)) {
     groups[[i]] <- (((i-1):(i+1) - 1 + n) %% n) + 1
 }
 
@@ -23,12 +22,22 @@ r = inla(formula, family="binomial", Ntrials=n, data=Tokyo,
          control.inla = list(int.strategy = "eb"), 
          control.compute = list(cpo = T, 
                                 control.gcpo = list(enable = TRUE,
-                                                    group.size = 1, 
-                                                    verbose = !TRUE, 
-                                                    ## either of these give the same result
-                                                    ##groups = groups
+                                                    num.level.sets = 2, 
+                                                    verbose = TRUE, 
                                                     selection = selection
                                                     )))
+rr = inla(formula, family="binomial", Ntrials=n, data=Tokyo,
+         verbose = TRUE,
+         inla.mode = "experimental",
+         num.threads = "1:1",
+         control.inla = list(int.strategy = "eb"), 
+         control.compute = list(cpo = T, 
+                                control.gcpo = list(enable = TRUE,
+                                                    num.level.sets = 2, 
+                                                    verbose = TRUE, 
+                                                    groups = groups
+                                                    )))
 
-plot(r$cpo$cpo, r$gcpo$gcpo, pch = 1)
-abline(a = 0, b = 1, lwd = 2)
+res <- cbind(seq_along(r$gcpo$gcpo),  r$gcpo$gcpo, rr$gcpo$gcpo)
+res[!is.na(res[, 2]), ]
+
