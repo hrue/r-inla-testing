1317f5ab40522e92384009577e1d999dff489f36
Haavard Rue <haavard.rue@kaust.edu.sa>
2022-09-09 21:02:12 -0700

    new

---
+++ test-speed/spde-toy2/runme.R
@@ -0,0 +1,20 @@
+inla.setOption(inla.call = NULL,
+               num.threads = "4:1")
+first.time <- TRUE
+source("spde_nsexample.R")
+orig <- c(fitns0$cpu[2], 
+          fitns1$cpu[2], 
+          fitns2$cpu[2])
+orig.mlik <- c(fitns0$mlik, 
+               fitns1$mlik, 
+               fitns2$mlik)
+
+inla.setOption(inla.call = 'inla.mkl.work')
+first.time <- FALSE
+source("spde_nsexample.R")
+new <- c(fitns0$cpu[2], 
+          fitns1$cpu[2], 
+          fitns2$cpu[2])
+new.mlik <- c(fitns0$mlik, 
+              fitns1$mlik, 
+              fitns2$mlik)
