1c560184ad45184d33b558954f063d5d0d8e5552
Haavard Rue <haavard.rue@kaust.edu.sa>
2022-09-27 12:06:52 +0300

    new

--- test-gcpo/runme17.R
+++ test-gcpo/runme17.R
@@ -1,5 +1,6 @@
 library(INLA)
-inla.setOption(inla.mode = "classic")
+INLA:::inla.my.update()
+inla.setOption(inla.mode = "experimental")
 inla.setOption(inla.call = "inla.mkl.work")
 
 data(Germany)
@@ -12,5 +13,10 @@ formula3 = Y ~ f(region, model="besag", graph=g) + f(x, model="rw2")
 r = inla(formula3,
          family="poisson",
          data=Germany,
+         control.compute = list(control.gcpo = list(enable = TRUE)), 
+         safe = FALSE, 
          E=E,
          verbose = TRUE)
+
+r1 <- inla.group.cv(r, num.level.sets = 1)
+##r2 <- inla.group.cv(r, num.level.sets = 2)
