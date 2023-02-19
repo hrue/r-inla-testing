0cda60cc2f63618a2e61478db2bf5a0b50d42873
Haavard Rue <haavard.rue@kaust.edu.sa>
2023-02-19 11:39:04 +0300

    new

--- test-config/runme6.R
+++ test-config/runme6.R
@@ -1,14 +1,24 @@
+INLA:::inla.my.update()
+inla.setOption(inla.call = "inla.mkl.work")
+
 set.seed(1234)
+m <- 10
 n <- 10
-A <- diag(n)
+A <- diag(1, m, n)
 x <- rnorm(n, sd = 0.2)
 eta <-  -1 + x
-y <- rpois(n, exp(eta))
+y <- rpois(m, exp(A %*% eta))
 
 r <- inla(y  ~ 1 + x,
-          data = data.frame(y, x),
+          data = list(y = y, x = x, A = A),
           family = "poisson",
           control.compute = list(config = TRUE),
-          control.predictor = list(A = A), 
+          control.predictor = list(A = A, compute = TRUE), 
+          control.inla = list(control.vb = list(enable = FALSE)))
+rr <- inla(y  ~ 1 + x,
+          data = list(y = y, x = x, A = A),
+          family = "poisson",
+          control.compute = list(config = TRUE),
+          control.predictor = list(compute = TRUE), 
           control.inla = list(control.vb = list(enable = FALSE)))
            
