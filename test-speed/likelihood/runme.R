0c9e53478dae1dbf84972c299c55908fa6a29292
Haavard Rue <haavard.rue@kaust.edu.sa>
2024-10-14 20:55:25 +0300

    new

---
+++ test-speed/likelihood/runme.R
@@ -0,0 +1,80 @@
+n <- 10^5
+inla.setOption(num.threads = 1, safe = FALSE, verbose = FALSE)
+cat(sep="", "\n")
+
+y <- rnorm(n)
+r <- inla(y ~ 1, data = data.frame(y), family = "stdnormal")
+cat(sep="", "stdnormal ", r$cpu.used[2], "\n")
+
+for(link in c("logit", "probit", "cloglog"))
+{
+    y <- rbinom(n, size = 1, prob = 0.5)
+    r <- inla(y ~ 1, data = data.frame(y),
+              family = "binomial",
+              control.family = list(
+                  link = link))
+    cat(sep="", "binomial(size=1,", link, ") ", r$cpu.used[2], "\n")
+
+    y <- rbinom(n, size = 10, prob = 0.5)
+    r <- inla(y ~ 1, data = data.frame(y, m = 10),
+              Ntrials = m,
+              family = "binomial", 
+              control.family = list(
+                  link = link))
+    cat(sep="", "binomial(size=10,", link, ") ", r$cpu.used[2], "\n")
+
+    y <- rbinom(n, size = 100, prob = 0.5)
+    r <- inla(y ~ 1, data = data.frame(y, m = 100),
+              Ntrials = m, family = "binomial", 
+              control.family = list(
+                  link = link))
+    cat(sep="", "binomial(size=100,", link, ") ", r$cpu.used[2], "\n")
+
+    y <- rbinom(n, size = 1, prob = 0.5)
+    r <- inla(y ~ 1, data = data.frame(y),
+              family = "betabinomial",
+              control.family = list(
+                  hyper = list(rho = list(fixed = TRUE)), 
+                  link = link))
+    cat(sep="", "betabinomial(size=1,", link, ") ", r$cpu.used[2], "\n")
+
+    y <- rbinom(n, size = 10, prob = 0.5)
+    r <- inla(y ~ 1, data = data.frame(y, m = 10),
+              Ntrials = m, family = "betabinomial", 
+              control.family = list(
+                  hyper = list(rho = list(fixed = TRUE)), 
+                  link = link))
+    cat(sep="","betabinomial(size=10,", link, ") ", r$cpu.used[2], "\n")
+
+    y <- rbinom(n, size = 100, prob = 0.5)
+    r <- inla(y ~ 1, data = data.frame(y, m = 100),
+              Ntrials = m, family = "betabinomial", 
+              control.family = list(
+                  hyper = list(rho = list(fixed = TRUE)), 
+                  link = link))
+    cat(sep="", "betabinomial(size=100,", link, ") ", r$cpu.used[2], "\n")
+}
+
+y <- rpois(n, 1)
+r <- inla(y ~ 1, data = data.frame(y),
+          family = "poisson")
+cat(sep="", "poisson ", r$cpu.used[2], "\n")
+
+y <- rgamma(n, shape = 1, rate = 1)
+r <- inla(y ~ 1, data = data.frame(y),
+          family = "gamma",
+          control.family = list(
+              hyper = list(prec = list(initial = 0,
+                                       fixed = TRUE))))
+cat(sep="", "gamma ", r$cpu.used[2], "\n")
+
+y <- rgamma(n, shape = 1, rate = 1)
+r <- inla(y ~ 1, data = data.frame(y),
+          family = "gamma",
+          control.family = list(
+              hyper = list(prec = list(initial = 0,
+                                       fixed = TRUE)),
+              control.link = list(
+                  model = "quantile",
+                  quantile = 0.5)))
+cat(sep="", "gamma(quantile) ", r$cpu.used[2], "\n")
