df4eb2e5718ecacd2e891002a362ffb16a878528
Haavard Rue <haavard.rue@kaust.edu.sa>
2025-12-01 09:56:57 +0300

    new

--- test-vm/runme2.R
+++ test-vm/runme2.R
@@ -1,26 +1,27 @@
 library(circular)
 
-lfun <- function(x) 2 * pi * (1/(1+exp(-x)) - 0.5)
-lfun <- function(x) 2 * pi * (1/(1+exp(-x))) - pi
-lfuninv <- function(x) log((pi + x)/(pi - x))
+INLA:::inla.my.update(b = T)
 
-n <- 300
-x <- rnorm(n, sd = 0.2)
+lfun <- function(x) 2 * atan(x)
+lfuninv <- function(x) tan(x/2)
+
+n <- 3
+x <- rnorm(n, sd = 0.1)
 x <- x - mean(x)
-mu <- lfun(0.1 + x)
+mu <- lfun(x)
 kappa <- 10
 y <- numeric(n)
-for(i in 1:n) {
-    y[i] <- rvonmises(1, circular(mu[i]), kappa)
-}
-r <- inla(y ~ 1 + x,
+y[1] <- -0.1
+y[2] <- 0
+y[3] <- 0.1
+
+r <- inla(y ~ -1 + x,
           data = data.frame(y, x),
-          family = "vm",
+          family = "lavm",
           control.inla = list(cmin = 0), 
-          control.family = list(link = "tan.pi",
+          control.family = list(link = "tan",
                                 hyper = list(
                                     prec = list(initial = log(kappa),
-                                                fixed = !TRUE))), 
+                                                fixed = TRUE))), 
           verbose = TRUE)
-summary(r)
 
